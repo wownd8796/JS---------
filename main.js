9ea7432358690e963f2e6ac8458e2978487f1a1c
wownd8796 <wownd8796@gmail.com>
2023-02-09 20:49:53 +0900

    20230209

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-<script
+{
+  /* <script
       src="https://code.jquery.com/jquery-3.6.3.js"
       integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
       crossorigin="anonymous"
@@ -12,4 +13,5 @@
       }).done(function (msg) {
         alert("Data Saved: " + msg);
       });
-    </script>
+    </script> */
+}
